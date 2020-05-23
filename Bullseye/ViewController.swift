fa90375b27d17cd7b1701624ec4af2c761d43dc2
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-05-22 22:34:51 -0400

    Add UI items & include action events for the items

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 50
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert() {
+        let message = "The value of the slider is \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hey, user", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = lroundf(slider.value)
+    }
+    
 }
 
