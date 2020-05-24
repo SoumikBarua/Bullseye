11c3da224120d154757ae771af2e5fd2e37f0040
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-05-23 23:53:21 -0400

    Set up slider outlet & generate random number

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 50
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    var targetValue = 0
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startNewRound()
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is \(currentValue)"
+        let message = "The value of the slider is \(currentValue)" + "\nThe target value is \(targetValue)"
         
         let alert = UIAlertController(title: "Hey, user", message: message, preferredStyle: .alert)
         
@@ -27,10 +31,25 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider){
         currentValue = lroundf(slider.value)
+    }
+    
+    
+    // This starts a new round with a new random target number
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
     
 }
