b54e4af9aec989f4680089d6f4707b809c01bcb6
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-05-27 23:58:27 -0400

    Add view controller & screen for About page

---
+++ Bullseye/AboutViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AboutViewController.swift
+//  Bullseye
+//
+//  Created by SB on 5/27/20.
+//  Copyright © 2020 SB. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeAboutView() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
